2a49b35a8462d8ab60991bf98b07f55067708ce7
Justina Kuchta <justyna.kuchta@gmail.com>
2020-09-30 15:20:30 -0400

    added expo-permissions and logic to check for and ask permission

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useEffect} from 'react';
+import { StyleSheet, View, Button } from 'react-native';
+import * as Notifications from 'expo-notifications'
+import * as Permissions from 'expo-permissions'
 
 export default function App() {
+
+  // check for permission and ask for it if not granted
+  useEffect(() => {
+    Permissions.getAsync(Permissions.NOTIFICATIONS).then(statusObj => {
+      if (statusObj.status !== 'granted') {
+        return Permissions.askAsync(Permissions.NOTIFICATIONS)
+      }
+    }).then(statusObj => {
+      if (statusObj.status !== 'granted') {
+        return
+      }
+    })
+  }, [])
+
+  const triggerNotificationHandler = () => {
+    Notifications.scheduleNotificationAsync({
+      content: {
+        title: 'My first local notification',
+        body: 'This is the very first local notification yeet'
+      },
+      trigger: {
+        seconds: 5
+      }
+    })
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button title="Trigger Notifications" onPress={triggerNotificationHandler} />
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +45,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
